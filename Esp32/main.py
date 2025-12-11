f58213d7adec4929938a42bfa5a033bf541274f3
Thomas Larsen <larsen.thomas.dk@gmail.com>
2025-12-11 12:15:31 +0100

    Updated comments

--- Esp32/main.py
+++ Esp32/main.py
@@ -3,15 +3,15 @@ import socket
 import machine
 import dht
 import time
-import ujson  # We use JSON so the data is easy to read later
+import ujson
 
 # --- CONFIGURATION ---
 SSID = 'MGV2-DMU1'
 PASSWORD = 'lanmagle'
-BROADCAST_IP = '255.255.255.255' # This sends to everyone on the network
-PORT = 37020                      # The "channel" we are shouting on
-PIN_NUM = 4                    # Change this if not using Pin 4
-ID = 1
+BROADCAST_IP = '255.255.255.255'  # This sends to everyone on the network
+PORT = 37020                      # Broadcast port
+PIN_NUM = 4                       # Pin where the DHT11 is connected
+ID = 1                            # Unique ID for this sensor        
 
 # --- SETUP WIFI ---
 wlan = network.WLAN(network.STA_IF)
@@ -25,18 +25,19 @@ while not wlan.isconnected():
 print("\nConnected! IP:", wlan.ifconfig()[0])
 
 # --- SETUP UDP SOCKET ---
-# SOCK_DGRAM means UDP
-# SO_BROADCAST allows us to send to 255.255.255.255
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+# Broadcast data every 5 seconds
 try:
     while True:
+        # Read data from DHT11 sensor
         sensor = dht.DHT11(machine.Pin(PIN_NUM))
         sensor.measure()
         temp = sensor.temperature()
         hum = sensor.humidity()
         
+        # Prepare payload
         payload_data = {
             "type": "dht11",
             "id": ID,
@@ -45,6 +46,7 @@ try:
             "last_updated": time.time()
         }
         
+        # Serialize to JSON and encode to bytes
         json_string = ujson.dumps(payload_data)
         encoded_data = json_string.encode('utf-8')
         
@@ -52,7 +54,6 @@ try:
         sock.sendto(encoded_data, (BROADCAST_IP, PORT))
         
         time.sleep(5)
-        
 except KeyboardInterrupt:
     print("\n\n Broadcast stopped by user (Ctrl+C).")
 except Exception as e:
